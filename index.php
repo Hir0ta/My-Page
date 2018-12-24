d2e09fdf22a3c51952f95884c68cc6160766eff5
HirOta-kun <kis.zoltan.cs@gmail.com>
2018-12-24 11:04:15 +0100

    added new feature

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Hir0ta</title>
     <link rel="icon" href="https://i.ibb.co/w7wdZJf/HirOta.png">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <link rel="stylesheet" type="text/css" href="style.css">
     <?php
       //error handle
@@ -13,21 +14,17 @@
       //include style for the additional content
       print '<link rel="stylesheet" type="text/css" href="' . $_GET["lap"] . '/style.css">';
     ?>
-    
     <!--For icons-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
     <!-- jQuery library -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <!-- Bootstrap library -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!-- Popper JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <!-- Latest compiled JavaScript -->
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script>
-      /*for JQuery scripts*/
-
-    </script>
     <style>
       *{box-sizing: border-box;}
     </style>
@@ -51,7 +48,7 @@
             </a>
             <div class="dropdown-menu bg-dark" aria-labelledby="dropdown_target">
               <ul class="navbar-nav">
-                <a href="index.php?lap=#" class="dropdown-item text-light bg-dark">###</a>
+                <a href="index.php?lap=business_card" class="dropdown-item text-light bg-dark">Business Card</a>
                 <a href="index.php?lap=#" class="dropdown-item text-light bg-dark">###</a>
                 <a href="index.php?lap=#" class="dropdown-item text-light bg-dark">###</a>
                 <a href="index.php?lap=#" class="dropdown-item text-light bg-dark">###</a>
@@ -67,7 +64,6 @@
             <div class="dropdown-menu bg-dark" aria-labelledby="dropdown_target">
               <ul class="navbar-nav">
                 <a href="index.php?lap=broodcomb" class="dropdown-item text-light bg-dark">Brood Comb</a>
-                <a href="index.php?lap=business_card" class="dropdown-item text-light bg-dark">Business Card</a>
                 <a href="index.php?lap=#" class="dropdown-item text-light bg-dark">###</a>
                 <a href="index.php?lap=#" class="dropdown-item text-light bg-dark">###</a>
                 <a href="index.php?lap=#" class="dropdown-item text-light bg-dark">###</a>
