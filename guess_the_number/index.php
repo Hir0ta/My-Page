34102fc79c094db7ef3831454e59448b28548b74
HirOta-kun <kis.zoltan.cs@gmail.com>
2018-12-25 18:06:16 +0100

    added new feature and debugged bootstrap

--- guess_the_number/index.php
+++ guess_the_number/index.php
@@ -1,22 +1,13 @@
-  <div class="panel-body text-danger">
-    <div class="row">
-      <div class="content col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <h1>Wanna play a game?</h1>
-        <p>I tell you what's gonna be: I think about a number between 1 and 100! You will try to guess it! You will have 10 try! I will even tell you if your guess is low or hi! If you can't guess it correctly on the 10th try...well you can restart the game! ;) </p>
-        <h2>Let's the game begin!<h2>
-        <br><br>
-      </div>
-      <br><br>
-      <div class="content col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        <p class = "guesses"></p>
-        <p class = "lastResult"></p>
-        <p class = "lowOrHi"></p>
-      </div>
-      <div class="field content col-xs-12 col-sm-12 col-md-12 col-lg-12 d-flex justify-content-center">
-        <label for="guessField">Enter a guess:</label>
-      </div>
-      <div class="field col-xs-10 col-sm-10 col-md-12 col-lg-12 d-flex justify-content-center">
-        <input type="text" id="guessField" class="guessField">
+  <div class="panel-body">
+    <h1>Wanna play a game?</h1>
+    <p>I tell you what's gonna be: I think about a number between 1 and 100! You will try to guess it! You will have 10 try! <br> I will even tell you if your guess is low or high! If you can't guess it correctly on the 10th try...well you can restart the game! ;) </p>
+    <h2>Let's the game begin!<h2>
+    <div class="guessContainer">
+      <p class = "guesses"></p>
+      <p class = "lastResult"></p>
+      <p class = "lowOrHi"></p>
+      <div class="field">
+        <label for="guessField">Enter a guess: </label><input type="text" id="guessField" class="guessField">
         <input type="submit" value="Submit" class="guessSubmit">
       </div>
     </div>
@@ -70,9 +61,6 @@
         guessSubmit.disabled = true;
         resetButton = document.createElement("button");
         resetButton.textContent = "Start new game";
-        //resetButton.id = "::reset";
-        //resetButton.style.cssText = "margin-left: 1rem";
-        //document.getElementById("reset.Write").appendChild(resetButton);
         document.body.appendChild(resetButton);
         resetButton.addEventListener("click",resetGame);
       }
@@ -88,7 +76,9 @@
         guessSubmit.disabled = false;
         guessField.value = "";
         guessField.focus();
-        lastResult.style.backgroundColor = "white";
+        lastResult.style.backgroundColor = "";
+        lastResult.textContent = "";
+        guesses.textContent = "";
         randomNumber = Math.floor(Math.random()*100) +1;
         //debug
         //console.log(randomNumber);
